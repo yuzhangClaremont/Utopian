b9897e35a77c815bc5701180e18c53208eccfd13
Yun <yuzhang@g.hmc.edu>
2018-06-22 01:30:16 +0800

    added login

---
+++ readme.md
@@ -0,0 +1,34 @@
+Utopian Rainbow
+
+# User registration
+https://www.youtube.com/watch?v=8aTnmsDMldY
+
+## intro to Flask-login
+pip install flask-login
+
+### setup database
+pip install Flask-SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+
+## install Flask-WTF
+http://flask-wtf.readthedocs.io/en/stable/install.html
+$ pip install Flask-WTF
+
+## install flask-bootstrap
+pip install flask-bootstrap 
+
+# SQLite
+sqlite3 test.db
+create table employees(id integer primary key, name text);
+
+.help
+.databases
+.tables
+.exit
+ctrl+d exit
+
+>>> from yourapplication import db
+>>> db.create_all() 
+sqlite3 database.db
+.table
+sqlite> select * from user;
--- utopianRainbow.py
+++ utopianRainbow.py
@@ -1,6 +1,36 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+# from flask_mysql import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+
+# from passlib.hash import sha256_crypt
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='Thisissecret'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////Users/yunzhang/Documents/computer Science/UTOPIAN/database.db'
+Bootstrap(app)
+db = SQLAlchemy(app)
 # app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(15),unique=True)
+    email=db.Column(db.String(50),unique=True)
+    password = db.Column(db.String(80))
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(),Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8,max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email',validators=[InputRequired(),Email(message='Invalid email'),Length(max=50)])
+    username = StringField('username', validators=[InputRequired(),Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8,max=80)])
+  
 
 @app.route("/")
 def home():
@@ -22,17 +52,54 @@ def fellowship():
 def community():
     return render_template("community.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
-@app.route("/signup")
+@app.route("/login", methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        # first result in database, username suppost to be unique
+        if user:
+            if user.password== form.password.data:
+                return redirect(url_for('dashboard'))
+        return 'invalid username or password'
+    return render_template("login.html", form=form)
+
+@app.route("/signup", methods=['GET','POST'])
 def signup():
-    return render_template("signup.html")
+    form=RegisterForm()
+    if form.validate_on_submit():
+        new_user= User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        return '<h1>'+ form.email.data +'</h1>'
+
+    return render_template("signup.html",form=form)
 
 @app.route("/chineseIndex")
 def chineseIndex():
     return render_template("chineseIndex.html")
+
+# https://wtforms.readthedocs.io/en/stable/
+
+# class RegisterForm(Form):
+#     username= StringField('username',[validators.Length(min=4,max=25)])
+#     email = StringField('Email', [validators.Length(min=6,max=50)])
+#     password = PasswordField('Password', [
+#         validators.DataRequired(),
+#         validators.EqualTo('confirm',message='passwords do not match')
+#     ])
+#     confirm = PasswordField('confirm password')
+
+# @app.route('/register',methoods=['GET','POST'])
+# def register():
+#     form = RegisterForm(request.form)
+#     if request.method == 'POST' and form.validate():
+
+#         return render_template('register.html',form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
